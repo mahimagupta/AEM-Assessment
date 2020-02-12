5e04c25c18981af6ec87949779a1ffd1f8554599
Mahima Gupta <mahimagupta2795@gmail.com>
2020-02-12 13:55:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,7 @@ const app = express()
 const PORT = 8080
 
 app.get('/romannumeral', function (req, res) {
-    
-    //  const {number} = req.query
-    //     try {
-    //         if (!number) throw Error('No number provided')
-    //         // const romanNumeral = getRomanNum(+number)
-	// 		// return res.json({romanNumeral})
-	// 		// const answer = getRomanNum(req.query.query)
-    // 		// return res.json({answer})
-    //     } catch(e) {
-    //         e.stack && console.log(e.stack)
-    //         res.status(400).json({
-    //             error: `Failed to convert to Roman Numeral because: ${e.message || e}`,
-    //         })
-	// 	}
-		
+    		
     	const answer = getRomanNum(req.query.query)
     	return res.json({answer})
 })
